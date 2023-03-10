7fe7a9337eca0414c41d0581f4986020b7441a9e
John <vannok@live.com>
2023-03-11 00:25:42 +0300

    16th commit

--- server/dist/utils/logger.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const winston_1 = require("winston");
-const { combine, colorize, simple } = winston_1.format;
-const log = (0, winston_1.createLogger)({
-    format: combine(colorize(), simple()),
-    transports: [new winston_1.transports.Console()],
-});
-exports.default = log;
