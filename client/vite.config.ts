5c0db02d71dcbf9f6c0e4d3e5b6c829a0524e1c9
John <vannok@live.com>
2023-04-14 12:15:13 +0300

    books part

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://[::1]:1337",
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+    Object.assign(process.env, loadEnv(mode, process.cwd()));
+    return {
+        base: process.env.VITE_API_KEY,
+        plugins: [react()],
+        server: {
+            proxy: {
+                "/api": {
+                    target: "http://[::1]:1337",
+                },
+            },
+        },
+    };
 });
